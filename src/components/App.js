732102317ce2454eed59dfac161b3098580915b0
sachin7con <sachin7con@gmail.com>
2024-12-29 00:44:01 +0530

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
+//SGN, ONSPN, JSSR, JBB, JSLV, JMD, JSM, JSSR, JSRK, JMD
+import "regenerator-runtime/runtime"; // Import at the top
 
-import React from "react";
+import React, { useState, useMemo, useEffect } from "react";
+import axios from "axios";
 import './../styles/App.css';
 
 const App = () => {
+     const [fetchedData, setFetchedData] = useState([]);
+     const [loading, setLoading] = useState(true);
+
+  
+   const memoizedData = useMemo(() => {
+        return fetchedData;
+   }, [fetchedData]);
+     
+   useEffect(() => {
+    const fetchingData = async () => {
+      setLoading(true);
+      try {
+        const fetchData = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
+        setFetchedData(fetchData.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchingData();
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/*ONS Do not remove the main div */}
+      <ul>
+        {loading ? (<p>Loading ...</p>) : (memoizedData.map((fd, id) => (
+          <li key={id}><h3>{fd.title}</h3> <p>{fd.body}</p></li>
+        )))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
